7db66b9ee1fa4abaa4dbf85477dee9cf3e678561
Brian Lalonde <brianary@gmail.com>
2025-03-13 22:29:14 -0700

    🐛 Fix publish job
    - resolves #3

--- Publish-ThisModule.ps1
+++ Publish-ThisModule.ps1
@@ -3,17 +3,22 @@
 [Parameter(Mandatory=$true)][string] $BuildConfiguration,
 [Parameter(Mandatory=$true)][string] $GalleryKey
 )
+"- &#x1F3D7; Build config $BuildConfiguration" >> $GITHUB_STEP_SUMMARY
 dotnet publish -c:$BuildConfiguration
-$proj = Get-ChildItem -Filter *.??proj -Recurse
-$MSBuildProjectName = $proj.BaseName
-Join-Path ($env:PSModulePath -split ';') $MSBuildProjectName |
+$proj = Get-ChildItem -Filter *.??proj -Recurse |Select-Object -First 1
+$Name = $proj.BaseName
+"::notice file=$($proj.FullName),title=${Name}::Project"
+Join-Path ($env:PSModulePath -split ';') $Name |
     Where-Object {Test-Path $_ -Type Container} |
     Remove-Item -Recurse -Force -Verbose
 Get-ChildItem $proj.DirectoryName -Filter publish -Directory -Recurse |Push-Location
-Import-LocalizedData Module -FileName $MSBuildProjectName -BaseDirectory "$PWD"
+Import-LocalizedData Module -FileName $Name -BaseDirectory "$PWD"
 $Version = $Module.ModuleVersion
-$InstallPath = Join-Path $env:UserProfile Documents PowerShell Modules $MSBuildProjectName $Version
+$InstallPath = Join-Path $env:UserProfile Documents PowerShell Modules $Name $Version
 if(!(Test-Path $InstallPath -Type Container)) {New-Item -Type Directory $InstallPath}
-Copy-Item * -Destination $InstallPath
+"- &#x1F4E6; Stage to $InstallPath" >> $GITHUB_STEP_SUMMARY
+Copy-Item * -Destination $InstallPath -Verbose
+"::notice file=$InstallPath\$Name.psd1,title=${Name}::Installed module"
 Pop-Location
-Publish-Module -Name $MSBuildProjectName -NuGetApiKey $GalleryKey
+"- :rocket: Publish $Name v$Version"
+Publish-Module -Name $Name -NuGetApiKey $GalleryKey
