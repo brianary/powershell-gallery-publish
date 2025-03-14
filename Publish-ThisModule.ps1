bcce46177c29bd9c07e88b8c0d3ef59c69c6730c
Brian Lalonde <brianary@gmail.com>
2025-03-14 15:40:34 -0700

    🎨 Minor formatting changes

--- Publish-ThisModule.ps1
+++ Publish-ThisModule.ps1
@@ -9,16 +9,17 @@ $proj = Get-ChildItem -Filter *.??proj -Recurse |Select-Object -First 1
 $Name = $proj.BaseName
 "::notice file=$($proj.FullName),title=${Name}::Project"
 Join-Path ($env:PSModulePath -split ';') $Name |
-    Where-Object {Test-Path $_ -Type Container} |
-    Remove-Item -Recurse -Force -Verbose
+	Where-Object {Test-Path $_ -Type Container} |
+	Remove-Item -Recurse -Force -Verbose
 Get-ChildItem $proj.DirectoryName -Filter publish -Directory -Recurse |Push-Location
 Import-LocalizedData Module -FileName $Name -BaseDirectory "$PWD"
 $Version = $Module.ModuleVersion
 $InstallPath = Join-Path $env:UserProfile Documents PowerShell Modules $Name $Version
-if(!(Test-Path $InstallPath -Type Container)) {New-Item -Type Directory $InstallPath}
+if(!(Test-Path $InstallPath -Type Container)) {New-Item -Type Directory $InstallPath |Out-Null}
 "- &#x1F4E6; Stage to $InstallPath" >> $GITHUB_STEP_SUMMARY
 Copy-Item * -Destination $InstallPath -Verbose
 "::notice file=$InstallPath\$Name.psd1,title=${Name}::Installed module"
 Pop-Location
 "- :rocket: Publish $Name v$Version" >> $GITHUB_STEP_SUMMARY
+"::notice file=$GITHUB_STEP_SUMMARY,title=Summary::Publish summary"
 Publish-Module -Name $Name -NuGetApiKey $GalleryKey
